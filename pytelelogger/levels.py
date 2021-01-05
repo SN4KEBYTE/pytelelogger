c97a50f93e58a75e040c1aeb8c1e994f1e133810
SN4KEBYTE <timurcat7@gmail.com>
2021-01-05 18:30:03 +0700

    Added docstrings. Fixed param name in has_value.

--- pytelelogger/levels.py
+++ pytelelogger/levels.py
@@ -1,7 +1,12 @@
-from enum import Enum
+from enum import Enum, unique
 
 
+@unique
 class TeleLoggerLevel(Enum):
+    """
+    Logging levels for TeleLogger.
+    """
+
     DEBUG = 0
     INFO = 1
     WARNING = 3
@@ -9,5 +14,13 @@ class TeleLoggerLevel(Enum):
     CRITICAL = 5
 
     @staticmethod
-    def has_value(item):
-        return item in [v.value for v in TeleLoggerLevel.__members__.values()]
+    def has_value(value: int) -> bool:
+        """
+        Check if provided int is valid logging level. For example, 10 is invalid level, but 3 is valid.
+        
+        :param value: value to be checked
+        
+        :return: True if value is valid logging level, False otherwise.
+        """
+
+        return value in [v.value for v in TeleLoggerLevel.__members__.values()]
