7b6fa28214e28f06564e24a2a6697d0a7aab9c41
SN4KEBYTE <timurcat7@gmail.com>
2021-01-05 18:30:45 +0700

    Now threaded decorator can be parametrized.

--- pytelelogger/threaded.py
+++ pytelelogger/threaded.py
@@ -1,9 +1,18 @@
 from threading import Thread
 
 
-def threaded(fun):
+def parametrized(dec):
+    def layer(*args, **kwargs):
+        def repl(f):
+            return dec(f, *args, **kwargs)
+        return repl
+    return layer
+
+
+@parametrized
+def threaded(fun, daemon=False):
     def wrapper(*args, **kwargs):
-        thread = Thread(target=fun, args=args, kwargs=kwargs)
+        thread = Thread(target=fun, daemon=daemon, args=args, kwargs=kwargs)
         thread.start()
 
         return thread
