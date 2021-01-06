d837458a9daa1a855ad262a70c267142414af8ca
SN4KEBYTE <timurcat7@gmail.com>
2021-01-06 17:16:37 +0700

    Added chat_id saving. Fixed stop method. Destructor now uses stop method. Splitted creation of TG log and file log. Some little codestyle fixes.

--- pytelelogger/telelogger.py
+++ pytelelogger/telelogger.py
@@ -1,17 +1,17 @@
-import yaml
-
 from datetime import datetime
 from queue import Queue
-from typing import Optional, TextIO, List, Dict
+from threading import Thread
+from typing import Optional, TextIO, Dict
 
+import yaml
 from telegram import Update
 from telegram.error import BadRequest
 from telegram.ext import Updater, CommandHandler, CallbackContext, Dispatcher
 
 from pytelelogger.defaults import TeleLoggerDefaults
 from pytelelogger.levels import TeleLoggerLevel
-from pytelelogger.types import PathType
 from pytelelogger.threaded import threaded
+from pytelelogger.types import PathType
 
 
 class TeleLogger:
@@ -26,7 +26,9 @@ class TeleLogger:
         :return: None.
         """
 
-        with open(cfg_path, 'r', encoding='utf-8') as f:
+        self.__cfg_path: PathType = cfg_path
+
+        with open(self.__cfg_path, 'r', encoding='utf-8') as f:
             cfg = yaml.safe_load(f)
 
         self.__updater: Updater = Updater(cfg['token'], *args, **kwargs)
@@ -45,7 +47,7 @@ class TeleLogger:
         self.__dtf: str = cfg.get('dtf', TeleLoggerDefaults.dtf)
         self.__emojis: Dict[str, str] = cfg.get('emojis', TeleLoggerDefaults.emojis)
 
-        self.__chat_id: Optional[int] = None
+        self.__chat_id: Optional[int] = cfg.get('chat_id')
         self.__log_queue: Queue = Queue()
 
         self.__updater.start_polling()
@@ -57,14 +59,7 @@ class TeleLogger:
         :return: None.
         """
 
-        self.__updater.stop()
-        self.__handler.join()
-
-        for k in self.__fstream:
-            try:
-                self.__fstream[k].close()
-            except AttributeError:
-                pass
+        self.stop(True)
 
     def __start(self, update: Update, context: CallbackContext) -> None:
         """
@@ -77,15 +72,28 @@ class TeleLogger:
         """
 
         if self.__username == update.effective_chat.username:
-            self.__chat_id = update.effective_chat.id
+            if self.__chat_id is None:
+                self.__chat_id = update.effective_chat.id
+
+                with open(self.__cfg_path, 'a') as f:
+                    f.write(f'\n\nchat_id: {self.__chat_id}')
 
             context.bot.send_message(chat_id=self.__chat_id, text=self.__greeting)
 
     def stop(self, force=False):
-        if not force:
-            self.__handler.join()
-
         self.__updater.stop()
+
+        if not force:
+            try:
+                self.__handler.join()
+            except AttributeError:
+                pass
+
+        for k in self.__fstream:
+            try:
+                self.__fstream[k].close()
+            except AttributeError:
+                pass
 
     @property
     def level(self) -> int:
@@ -94,7 +102,7 @@ class TeleLogger:
         
         :return: current logging level.
         """
-        
+
         return self.__level
 
     @level.setter
@@ -108,15 +116,15 @@ class TeleLogger:
         
         :raises: ValueError if provided level is < 0 or > 5.
         """
-        
+
         if not TeleLoggerLevel.has_value(level):
             raise ValueError('Unknown logging level')
 
         self.__level = level
 
-    def __create_log(self, level: str, message: str) -> str:
+    def __create_tg_log(self, level: str, message: str) -> str:
         """
-        Creates formatted log string.
+        Creates formatted log string for Telegram.
 
         :param level: logging level.
         :param message: message to be used in log.
@@ -124,7 +132,20 @@ class TeleLogger:
         :return: formatted log.
         """
 
-        return f'{self.__project}\n\n{level}\n{datetime.now().strftime(self.__dtf)}\n\n{message}\n'
+        return f'{self.__project}\n\n{self.__emojis[level]}{level.upper()}\n{datetime.now().strftime(self.__dtf)}\n\n' \
+               f'{message}\n\n{self.__generate_hashtags(level)}'
+
+    def __create_file_log(self, level: str, message: str) -> str:
+        """
+        Creates formatted log string for writing to file.
+
+        :param level: logging level.
+        :param message: message to be used in log.
+
+        :return: formatted log.
+        """
+
+        return f'[{level.upper()}:{datetime.now().strftime(self.__dtf)}] {message}'
 
     def __record_log(self, level: str, message: str) -> None:
         """
@@ -136,8 +157,8 @@ class TeleLogger:
         :return: None.
         """
 
-        log: str = self.__create_log(level, message)
         lvl: str = level.lower()
+        log: str = self.__create_file_log(lvl, message)
 
         try:
             self.__fstream[lvl if self.__mode == 'multi' else 'debug'].write(log)
@@ -145,15 +166,13 @@ class TeleLogger:
             pass
 
         if TeleLoggerLevel.__members__[level].value >= self.__level:
-            log += '\n' + '\n'.join(self.__generate_hashtags(lvl))
-            pos = log.find('\n\n') + 2
-            log = log[:pos] + self.__emojis[lvl] + log[pos:]
+            tg_log: str = self.__create_tg_log(lvl, message)
 
             try:
-                self.__updater.bot.send_message(chat_id=self.__chat_id, text=log)
+                self.__updater.bot.send_message(chat_id=self.__chat_id, text=tg_log)
             except BadRequest:
-                self.__log_queue.put(log)
-                self.__handler = self.__resend_logs()
+                self.__log_queue.put(tg_log)
+                self.__handler: Thread = self.__resend_logs()
 
     def debug(self, message: str) -> None:
         """
@@ -163,7 +182,7 @@ class TeleLogger:
         
         :return: None. 
         """
-        
+
         self.__record_log(TeleLoggerLevel.DEBUG.name, message)
 
     def info(self, message: str) -> None:
@@ -174,7 +193,7 @@ class TeleLogger:
         
         :return: None.
         """
-        
+
         self.__record_log(TeleLoggerLevel.INFO.name, message)
 
     def warning(self, message: str) -> None:
@@ -185,7 +204,7 @@ class TeleLogger:
         
         :return: None.
         """
-        
+
         self.__record_log(TeleLoggerLevel.WARNING.name, message)
 
     def error(self, message: str) -> None:
@@ -196,7 +215,7 @@ class TeleLogger:
         
         :return: None.
         """
-        
+
         self.__record_log(TeleLoggerLevel.ERROR.name, message)
 
     def critical(self, message: str) -> None:
@@ -207,10 +226,10 @@ class TeleLogger:
         
         :return: None.
         """
-        
+
         self.__record_log(TeleLoggerLevel.CRITICAL.name, message)
 
-    def __generate_hashtags(self, level: str) -> List[str]:
+    def __generate_hashtags(self, level: str) -> str:
         """
         Generate some hashtags to make search in Telegram dialog easier.
         
@@ -219,10 +238,10 @@ class TeleLogger:
         :return: list of 3 hashtags: #project, #project_level, #level
         """
 
-        return ['#' + h for h in [self.__project, self.__project + '_' + level, level]]
+        return '\n'.join(['#' + h for h in [self.__project, self.__project + '_' + level, level]])
 
     @threaded(daemon=True)
-    def __resend_logs(self) -> None:
+    def __resend_logs(self) -> Thread:
         """
         This method tries to resend all logs whose sending was messed up. Runs in separate thread.
         
