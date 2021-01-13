b9a6fb1e710d8f18bd2c0fef9de8bc4574efb684
Timur Kasimov <timurcat7@gmail.com>
2021-01-14 03:29:27 +0700

    Added __version__ and __all__ attributes.

---
+++ pytelelogger/__init__.py
@@ -0,0 +1,7 @@
+from pytelelogger.defaults import TeleLoggerDefaults
+from pytelelogger.levels import TeleLoggerLevel
+from pytelelogger.telelogger import TeleLogger
+
+__version__ = '0.1'
+
+__all__ = [__version__, 'TeleLoggerDefaults', 'TeleLoggerLevel', 'TeleLogger']
